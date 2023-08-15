cb8d6d903d4461c4f9eb37af879a67296035d2d7
JoshuaSiyame <josh2023decoded@gmail.com>
2023-08-15 21:16:57 +0300

    App commits

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,14 @@ import ContactMe from "./ContactMe/ContactMe";
 import Projects from "./Projects/Projects";
 import WhatIDo from "./WhatIDo/WhatIDo";
 import Skills from "./Skills/Skill";
+import AboutMe from "./AboutMe/AboutMe";
 
 // app component
 const App = () => {
 
     return (
         <div id="app-structure">
+            <AboutMe />
             <Skills />
             <WhatIDo />
             <Projects />
